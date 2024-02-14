14373e4a41d232cc5ce943e586f92e37bfc7a57e
nursaid <nursaid.seitkozhoev@mail.ru>
2024-02-14 16:26:18 +0600

    second

--- informatic.py
+++ informatic.py
@@ -12,28 +12,33 @@ longitude = 74.59
 spn = '0.5,0.5'
 
 
-url = f'https://search-maps.yandex.ru/v1/?apikey={api_key}&text={search_query}&ll={longitude},{latitude}&spn={spn}&results=80&lang=ru_RU'
+url = f'https://search-maps.yandex.ru/v1/?apikey={api_key}&text={search_query}&ll={longitude},{latitude}&spn={spn}&results=8&lang=ru_RU'
 
 cursor = conn.cursor()
 
 cursor.execute('''CREATE TABLE IF NOT EXISTS pharmacies
                   (id INTEGER PRIMARY KEY, name TEXT)''')
 
-
+new_name = set()
 response = requests.get(url)
 
 if response.status_code == 200:
     data = response.json()
     for result in data['features']:
         name = result['properties']['name']
-        cursor.execute("INSERT INTO pharmacies (name) VALUES (?)", (name,))
-        conn.commit()  
-
+        if name  not in new_name:
+            new_name.add(name)
+            cursor.execute("INSERT INTO pharmacies (name) VALUES (?)", (name,))
+            conn.commit()  
+            
 
 cursor.execute("SELECT * FROM pharmacies")
 rows = cursor.fetchall()
 for row in rows:
     print(row)
+    
+
+    
 
 
 cursor.close()
@@ -48,6 +53,8 @@ conn.close()
 
 
 
+# with open("new.csv",mode = "w") as new:
+#             new.write(new_name)
 
 
 
@@ -72,15 +79,3 @@ conn.close()
 
 
 
-
-
-# import sqlite3;
- 
-# sqlite3.connect('/home/nursaid/Рабочий стол/informatic/pharmacy.db', timeout=5.0, detect_types=0, isolation_level='DEFERRED', check_same_thread=True, factory=sqlite3.Connection, cached_statements=128, uri=False)
- 
-# con = sqlite3.connect("pharmacy.db")
-# cursor = con.cursor()
-
-
-
-# cursor.execute("INSERT INTO pharmacies (Field2) VALUES (?)", name )
